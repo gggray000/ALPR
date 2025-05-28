3a2640cd42cc6c79f06a8c464c4e4a588dbfc8bb
gggray000 <124888446+gggray000@users.noreply.github.com>
2025-05-28 12:24:28 +0200

    Wrapped preprocess methods

---
+++ preprocess/preprocessor.py
@@ -0,0 +1,15 @@
+import sys
+from edge_contours_detection import edge_detection, find_contours
+
+def preprocess(img_path):
+    image, edged = edge_detection(img_path)
+    output = find_contours(image, edged)
+    return output
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python run_preprocess.py <image_path>")
+        sys.exit(1)
+
+    image_path = sys.argv[1]
+    result = preprocess(image_path)
