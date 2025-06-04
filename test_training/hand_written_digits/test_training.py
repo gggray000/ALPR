8a0587cb58667aeeef46f5ea6a1363a713e6c9c7
gggray000 <124888446+gggray000@users.noreply.github.com>
2025-06-04 14:57:22 +0200

    Added dependencies for Nsight and Mlflow

--- test_training/hand_written_digits/test_training.py
+++ test_training/hand_written_digits/test_training.py
@@ -4,6 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from PIL import Image
+import nvtx
+import mlflow
+
+mlflow.set_tracking_uri(uri="http://localhost:5000")
+mlflow.set_experiment("hand_written_digits")
+
+class MLflowLogger(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        if logs is not None:
+            for key, value in logs.item():
+                mlfow.log_metric(key, value, step=epoch)
 
 # 1. Create vocabulary
 def build_vocab(filenames):
@@ -11,6 +22,7 @@ def build_vocab(filenames):
     return {c: i + 1 for i, c in enumerate(sorted(all_chars))}  # +1 to reserve 0 for padding
 
 # 2. Load images and labels
+@nvtx.annotate("Loading Training Data", color="blue")
 def load_dataset_from_filenames(folder_path, image_size=(128, 32)):
     images = []
     labels = []
@@ -44,9 +56,10 @@ def load_dataset_from_filenames(folder_path, image_size=(128, 32)):
 
     return images, padded_labels, char2idx
 
+@nvtx.annotate("Training Model", color="green")
 def train():
-    # mnist = tf.keras.datasets.mnist
-    # (x_train, y_train),(x_test, y_test) = mnist.load_data()
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train),(x_test, y_test) = mnist.load_data()
 
     train_path = '/home/stud3/Desktop/test_model_training/test_training/basic_CNN/dataset/train'
     val_path = '/home/stud3/Desktop/test_model_training/test_training/basic_CNN/dataset/val'
@@ -77,10 +90,8 @@ def train():
     print(f"Val size: {len(val_images)}")
     print(f"Test size: {len(test_images)}")
 
-    train_ds = tf.keras.utils.normalize(, axis=1)
+    train_ds = tf.keras.utils.normalize(train_ds, axis=1)
     x_test = tf.keras.utils.normalize(x_test, axis=1)
-
-    
 
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
@@ -90,34 +101,50 @@ def train():
 
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-    model.fit(x_train, y_train, epochs=300)
+    with mlflow.start_run():
+        mflow.log_param("epochs", 100)
+        mlflow.log_param("batch_size",32)
+        model.fit(trains_ds, 
+                  epochs=100,
+                  validation_data=val_ds,
+                  callbacks=[MLflowLogger])
+
+        loss, accurracy = model.evaluate(test_ds)
+        mlflow.log_metric("test_loss", loss)
+        mlflow.log_metric("test_accuracy", accuracy)
+        mlflow.tensorflow.log_model(model, "model")
+
+    # model.fit(x_train, y_train, epochs=100)
     model.summary()
     model.save('/home/stud3/Desktop/test_model_training/test_training/basic_CNN/plate_CNN_01.keras')
 
-    # after training
+    evaluate_model(x_test, y_test)
+
+@nvtx.annotate("Evaluating Trained Model", color="pink")
+def evaluate_model(x_test, y_test):
     model= tf.keras.models.load_model('/home/stud3/Desktop/test_model_training/test_training/basic_CNN/plate_CNN_01.keras')
     loss, accuracy = model.evaluate(x_test, y_test)
 
     print(loss)
     print(accuracy)
 
-# def apply(model):
-#     image_number = 1
-#     while os.path.isfile(f"/home/stud3/Desktop/test_model_training/test_training/digits/digit{image_number}.png"):
-#         try:
-#             img = cv2.imread(f"/home/stud3/Desktop/test_model_training/test_training/digits/digit{image_number}.png")[:,:,0]
-#             img = np.invert(np.array([img]))
-#             prediction = model.predict(img)
-#             # Giving the number with the highest activation. What is activation?
-#             print(f"The number is probably a {np.argmax(prediction)}")
-#             plt.imshow(img[0], cmap=plt.cm.binary)
-#             plt.show()
-#         except Exception as e:
-#             print(f"Error: {e}")
-#         finally:
-#             image_number += 1
+@nvtx.annotate("Inference", color="yellow")
+def apply(model):
+    image_number = 1
+    while os.path.isfile(f"/home/stud3/Desktop/test_model_training/test_training/digits/digit{image_number}.png"):
+        try:
+            img = cv2.imread(f"/home/stud3/Desktop/test_model_training/test_training/digits/digit{image_number}.png")[:,:,0]
+            img = np.invert(np.array([img]))
+            prediction = model.predict(img)
+            print(f"The number is probably a {np.argmax(prediction)}")
+            plt.imshow(img[0], cmap=plt.cm.binary)
+            plt.show()
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            image_number += 1
 
 if __name__ == '__main__':
-    #train()
+    train()
     model = tf.keras.models.load_model('/home/stud3/Desktop/test_model_training/test_training/handwritten.keras')
-    apply(model)
+    #apply(model)
