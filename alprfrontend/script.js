8be4f0489b6b0178fa72f5586ed869c851e67a9c
saniyah <saniyahfz@gmail.com>
2025-06-27 16:11:10 +0200

    Frontend

---
+++ alprfrontend/script.js
@@ -0,0 +1,40 @@
+let video = document.getElementById('video');
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+let snapButton = document.getElementById('snap');
+
+// Access the camera
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            video.srcObject = stream;
+            video.play();
+        });
+}
+
+// Snap button click
+snapButton.addEventListener("click", function () {
+    // Draw video frame to canvas
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    // Convert canvas to Blob and send via FormData
+    canvas.toBlob(function (blob) {
+        const formData = new FormData();
+        formData.append('image', blob, 'snapshot.jpg'); // Key must match Flask: 'image'
+
+        fetch('http://127.0.0.1:5002/predict', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+               console.log("Prediction:", data);
+                document.querySelector('.result').textContent = "Prediction: " + data.toString();
+                //alert("Predicted Digit: " + data.data);
+            })
+            .catch(error => {
+                console.error("Upload error:", error);
+                alert("Error uploading image.");
+            });
+    }, 'image/jpeg');
+});
