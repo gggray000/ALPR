3a495d2ae43e6169c12ed411bcaa887b7b932a53
gggray000 <124888446+gggray000@users.noreply.github.com>
2025-06-29 16:24:26 +0200

    Changed the logic of sending images to backend.

--- test_training/hand_written_digits/static/script.js
+++ test_training/hand_written_digits/static/script.js
@@ -11,25 +11,24 @@ fileInput.addEventListener("change", function () {
     reader.onload = function (event) {
         const img = new Image();
         img.onload = function () {
-            context.drawImage(img, 0, 0, canvas.width, canvas.height);
-            canvas.toBlob(function (blob) {
-                const formData = new FormData();
-                formData.append('image', blob, 'snapshot.jpg');
+            context.drawImage(img, 0, 0, 640, 480);
+        
+            const formData = new FormData();
+            formData.append('image', file); 
 
-                fetch("/alpr/predict", {
-                    method: 'POST',
-                    body: formData
-                })
-                .then(response => response.json())
-                .then(data => {
-                    console.log("Prediction:", data.prediction);
-                    document.getElementById('result').textContent = "Prediction: " + data.prediction;
-                })
-                .catch(error => {
-                    console.error("Upload error:", error);
-                    alert("Error: ", error);
-                });
-            }, 'image/jpeg');
+            fetch("/alpr/predict", {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Prediction:", data.prediction);
+                document.getElementById('result').textContent = "Prediction: " + data.prediction;
+            })
+            .catch(error => {
+                console.error("Upload error:", error);
+                alert("Error: ", error);
+            });
         };
 
         img.src = event.target.result;
